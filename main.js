a81e31b10ef2d4a56ef21c805ab4b26f579b8e97
roxyfaye <rdesantisnunally@gmail.com>
2017-06-08 16:38:06 -0400

    430

---
+++ main.js
@@ -0,0 +1,23 @@
+var container = document.querySelector('#container');
+
+var control = formData[0];
+console.log(control)
+
+var inputElement = document.createElement('input');
+inputElement.id = control.id;
+inputElement.placeholder = control.label;
+container.appendChild(inputElement)
+
+
+
+
+
+
+
+
+
+for(var index = 0; index < formData.length; index ++) {
+    
+}
+     
+
